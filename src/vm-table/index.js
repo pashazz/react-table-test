75126fbe12033445680e7f00bda9140cceefdc7c
Pavel Zinin <pzinin@gmail.com>
2018-04-23 23:51:07 +0300

    first commit

---
+++ src/vm-table/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class Table extends React.Component
+{
+    constructor()
+    {
+        super();
+        //this.x = input;
+        //axios.get()
+    }
+
+    render()
+    {
+        return (
+            //<div>{this.x}</div>
+            <div></div>
+        );
+    }
+}
+
+export default  Table;
